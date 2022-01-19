75b6bb30a2ae209f79a4ec92f6beed303f87a371
sulyman <sulymanabdullahi@gmail.com>
2022-01-19 09:52:57 +0100

    Feat:show  product details

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -6,40 +6,58 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 const Products = () => {
-  const { projectDetail, setProjectDetail } = useState("")
+  const [productDetail, setProductDetail] = useState("");
+  
+  const productToShow = products.find(
+    (product) => product.name === productDetail
+  );
+
+  const showProductDetail = (productName: string) => {
+    setProductDetail(productName);
+  };
+  
+
   return (
     <>
-      <div>
-        <h1>Select a products</h1>
-      </div>
-      <motion.div  
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}>
-        {products.map((product, index) => (
-          // eslint-disable-next-line @next/next/link-passhref
-          <Link key={index} href={""}>
-            <div className={styles.card} onClick={projectDetail}>
-              {/* <span className='drugs'>Pharmaceutical product</span> */}
+     
+      {productDetail === "" ? (
+        <div>
+          <div>
+            <h1>Select a products</h1>
+          </div>
 
-              <div className={styles.productInfo}>
-                
-                <Image
-                  // key={index}
-                  src={product.images}
-                  alt={"product" + (index + 1)}
-                  width={550}
-                  height={350}
-                />
-                <div className={styles.productDetails}>
-                <span>{product.name}</span>
-                <span>{product.price}</span>
+          <motion.div className={styles.productWrap}>
+            {products.map((product, index) => (
+              // eslint-disable-next-line @next/next/link-passhref
+              <Link key={index} href={""}>
+                <div
+                  className={styles.card}
+                  onClick={() => showProductDetail(product.name)}
+                >
+                  <div className={styles.productInfo}>
+                    <Image
+                      // key={index}
+                      src={product.images}
+                      alt={"product" + (index + 1)}
+                      width={550}
+                      height={350}
+                    />
+                    <div className={styles.productDetails}>
+                      <span>{product.name}</span>
+                      <span>{product.price}</span>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </motion.div>
+              </Link>
+            ))}
+          </motion.div>
+        </div>
+      ) : (
+        <div>
+          welcome to my products
+          {JSON.stringify(productToShow, null, 2)}
+        </div>
+      )}
     </>
   );
 };
--- src/utils/product.ts
+++ src/utils/product.ts
@@ -1,13 +1,15 @@
 export const products = [
   {
+    
     images: "/images/product1.png",
-    name:  "Mission pharma",
+    name:  "Pharma",
     price: "$700",
     desc: "Antibiotics are medicines that fight bacterial infections in people and animals. They work by killing the bacteria or by making it hard for the bacteria to grow and multiply. Antibiotics can be taken in different ways: Orally (by mouth).",
     
   },
   
   {
+    
     images: "/images/product2.jpg",
     name:  "Antibiotics",
     price: "$400",
